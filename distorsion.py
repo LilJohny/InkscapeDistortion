632f3b485f5891aab71778ea079616d04a77d22d
Denis <ivanenko@ucu.edu.ua>
2019-08-07 01:44:15 +0300

    Cleaned code

--- distorsion.py
+++ distorsion.py
@@ -1,12 +1,8 @@
 import inkex
 import os
-import sys
-import cubicsuperpath
 from math import sqrt
 import simplepath
-import simpletransform
 
-inkex.localize()
 
 
 class DistorsionExtension(inkex.Effect):
@@ -24,43 +20,16 @@ class DistorsionExtension(inkex.Effect):
                                      default=1.0,
                                      help="command line help")
 
-    #def effect(self):
-    #    self.svg_file = sys.argv[-1]
-    #    self.parse()
-    #    self.getoptions()
-    #    q = self.options.lambda_coef
-    #    element = self.getElementById(self.options.ids[0])
-    #    nodes = cubicsuperpath.parsePath(element.get('d'))
-    #    start, finish = element.attrib['d'].split(' ')[-2:]
-    #    self.getposinlayer()
-    #    x_c, y_c = self.view_center
-    #    width = int(self.getDocumentWidth().replace('mm', ''))
-    #    height = int(self.getDocumentHeight().replace('mm', ''))
-    #    distorted_nodes = []
-    #    for node in nodes[0]:
-    #        coordinates_distorted = []
-    #        for coordinates in node:
-    #            x_u = (coordinates[0] - x_c) / (width + height)
-    #            y_u = (coordinates[1] - y_c) / (width + height)
-    #            x_d = x_u / 2 / (q * y_u**2 + x_u**2 * q) * (
-    #                1 - sqrt(1 - 4 * q * y_u**2 - 4 * x_u**2 * q))
-    #            y_d = y_u / 2 / (q * y_u**2 + x_u**2 * q) * (
-    #                1 - sqrt(1 - 4 * q * y_u**2 - 4 * x_u**2 * q))
-    #            coordinates_distorted.append([x_d, y_d])
-    #        distorted_nodes.append(coordinates_distorted)
-    #    for distorted_node in distorted_nodes:
-    #        for distorted_coordinates in distorted_node:
-    #            distorted_coordinates[0] *= width + height
-    #            distorted_coordinates[0] += x_c
-    #            distorted_coordinates[1] *= width + height
-    #            distorted_coordinates[1] += y_c
-    #    distorted_path = cubicsuperpath.formatPath([distorted_nodes])
-    #    inkex.debug(element.get('d'))
-    #    inkex.debug(distorted_path)
-    #    element.set("d", distorted_path)
-    #    inkex.debug(dir(element))
-
     def distort_coordinates(self, x, y):
+        """Method applies barrel distorsion to given points with distorsion center in center of image, selected to 
+        
+        Args:
+            x (float): X coordinate of given point
+            y (float): Y coordinate of given point
+        
+        Returns:
+            tuple(float, float): Tuple with X,Y distorted coordinates of given point
+        """
         x_u = (x - self.x_c) / (self.width + self.height)
         y_u = (y - self.y_c) / (self.width + self.height)
         x_d = x_u / 2 / (self.q * y_u**2 + x_u**2 * self.q) * (
@@ -74,8 +43,6 @@ class DistorsionExtension(inkex.Effect):
         return x_d, y_d
 
     def effect(self):
-        #self.width = int(self.getDocumentWidth().replace('mm', ''))
-        #self.height = int(self.getDocumentHeight().replace('mm', ''))
         self.q = self.options.lambda_coef
         nodes = []
         for id, node in self.selected.iteritems():
@@ -90,10 +57,6 @@ class DistorsionExtension(inkex.Effect):
         self.height = max(y_coordinates) - min(y_coordinates)
         self.x_c = sum(x_coordinates) / len(x_coordinates)
         self.y_c = sum(y_coordinates) / len(y_coordinates)
-        self.x_c = 82.0
-        self.y_c = 279.0
-        inkex.debug(self.x_c)
-        inkex.debug(self.y_c)
         for id, node in self.selected.iteritems():
             if node.tag == inkex.addNS('path', 'svg'):
                 d = node.get('d')
